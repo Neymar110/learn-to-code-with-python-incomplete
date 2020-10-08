944b2152599b860c1283d836343168d58ec8c3d5
Neymar110 <lazerbeam1023@gmail.com>
2020-10-08 16:20:49 +0300

    git commit -m 10/8

---
+++ learn-to-code-with-python/25-Classes-Inheritance/name-mangling-for-privacy.py
@@ -0,0 +1,11 @@
+class Nonsense():
+    def __init__(self, name):
+        self.name = name
+    
+    def __rev(self):
+        print(self.name[::-1])
+
+class idk(Nonsense):
+    pass
+isaacs = Nonsense("Isaac")
+isaacs._Nonsense__rev()
---
+++ learn-to-code-with-python/25-Classes-Inheritance/polymorphism-I.py
@@ -0,0 +1,6 @@
+def percentage_func(num1, num2):
+    num3 = num2 / num1 * 100
+
+    print(f"{round(num3, 1)}%")
+
+percentage_func(17, 25)
